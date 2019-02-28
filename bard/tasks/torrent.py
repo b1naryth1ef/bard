5e70fc5e375725e701a48199e3a45b7c7653c3d8
Andrei <b1naryth1ef@gmail.com>
2019-02-27 16:31:06 -0800

    DRY up torrent extraction and cleanup some edge cases

--- bard/tasks/torrent.py
+++ bard/tasks/torrent.py
@@ -32,14 +32,34 @@ def update_torrents():
 
 
 def _get_result_path(torrent, ext='mkv'):
+    """
+    Returns the resulting path for a givent torrent file
+    """
     return os.path.join(
         bard.config['directories']['output'],
         torrent.episode.season.series.storage_name,
-        '{}.{}.mkv'.format(
+        '{}.{}.{}'.format(
             torrent.episode.season.series.storage_name,
             torrent.episode.season_episode_id,
+            ext
         )
     )
+
+
+def _store_torrent_media(torrent, source_path, keep=False):
+    final_destination_path = _get_result_path(torrent)
+    final_destination_dir = os.path.dirname(final_destination_path)
+
+    # If the path doesn't exist, attempt to make it
+    if not os.path.exists(final_destination_dir):
+        os.mkdir(final_destination_dir)
+
+    if keep:
+        shutil.copy(source_path, final_destination_path)
+    else:
+        os.rename(source_path, final_destination_path)
+
+    os.chmod(final_destination_path, 0777)
 
 
 @task()
@@ -48,34 +68,29 @@ def process_torrent(torrent):
 
     # Get the list of files
     files = bard.providers.fetch.get_files(torrent)
-    result_file = None
 
     # This really just means we tried to extract some media from the torrent
     torrent.processed = True
     torrent.save()
 
-    # First, check if this is compressed
+    # If the torrent contains a rar file attempt to unpack that
     rar_files = [i for i in files if i.endswith('.rar')]
     if len(rar_files) == 1:
         log.debug('Found rar file in torrent %s, unpacking...', torrent.id)
-        result_file = unpack_torrent(torrent, rar_files[0])
-        if not result_file:
-            return
+        unpack_torrent(torrent, rar_files[0])
+        return
     elif len(rar_files) > 1:
         log.error('Found multiple rar files in torrent %s (%s)', torrent.id, rar_files)
         return
-    else:
-        video_files = [i for i in files if i.endswith('mkv')]
 
-        if len(video_files) == 1:
-            log.debug('Found video file in torrent %s, moving and reverse symlinking', torrent.id)
-            source_path = os.path.join(bard.config['directories']['input'], video_files[0])
-            dest_path = _get_result_path(torrent)
-            shutil.copy(source_path, dest_path)
-            os.chmod(dest_path, 0777)
-            return
-
-        log.error('Found no rar file in torrent %s, skipping', torrent.id)
+    video_files = [i for i in files if i.endswith('mkv')]
+    if len(video_files) == 1:
+        log.debug('Found video file in torrent %s, moving and reverse symlinking', torrent.id)
+        source_path = os.path.join(bard.config['directories']['input'], video_files[0])
+        _store_torrent_media(torrent, source_path, keep=True)
+        return
+    elif len(video_files) > 1:
+        log.error('Found multiple video files in torrent %s (%s)', torrent.id, video_files)
         return
 
 
@@ -87,20 +102,11 @@ def unpack_torrent(torrent, rar_file):
         return
 
     rf = rarfile.RarFile(full_path)
-    names = [i for i in rf.namelist() if i.endswith('.mkv')]
-    if len(names) != 1:
-        log.error('Failed to find mkv file in namelist; %s', rf.namelist())
+
+    video_files = [i for i in rf.namelist() if i.endswith('.mkv')]
+    if len(video_files) != 1:
+        log.error('Failed to find video file to unpack from rar %s (%s)', torrent.id, rf.namelist())
         return
 
-    final_path = _get_result_path(torrent)
-    out_dir = os.path.dirname(final_path)
-
-    if not os.path.exists(out_dir):
-        os.mkdir(out_dir)
-
-    log.info('Attempting to unpack %s from %s to %s', names[0], full_path, out_dir)
-    rf.extract(names[0], out_dir)
-
-    os.rename(os.path.join(out_dir, names[0]), final_path)
-    os.chmod(final_path, 0777)
-    return final_path
+    rf.extract(video_files[0], os.path.dirname(full_path))
+    _store_torrent_media(torrent, os.path.join(os.path.dirname(full_path), video_files[0]))
